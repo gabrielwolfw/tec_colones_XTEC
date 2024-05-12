da4df37781c36a2a884b1a2cadeaefe630effa8d
LUIS ADRIAN PRENDAS ARIAS <lprendas@estudiantec.cr>
2024-05-12 11:41:05 -0600

    Se añadio el manejo de escepciones a administracion_sedes

--- administacion_sedes/centrosacopio.py
+++ administacion_sedes/centrosacopio.py
@@ -6,17 +6,24 @@ class CentrosAcopio:
         self.sedes_existentes = set()
         self.identificadores_existentes = set()
     
-    def validar_input_centro_acopio(self, sede, numero_contacto,identificador):
-        if not sede in self.sedes_existentes:
-            messagebox.showerror("Error", "La sede no existe")
+    def validar_input_centro_acopio(self, sede, numero_contacto, identificador):
+        try:
+            if not sede in self.sedes_existentes:
+                messagebox.showerror("Error", "La sede no existe")
+                return False
+            if not numero_contacto.isdigit() or not 1000000 < int(numero_contacto) < 100000000:
+                messagebox.showerror("Error", "El número de contacto debe ser un número debe tener 8 dígitos")
+                return False
+            if identificador in self.identificadores_existentes:
+                messagebox.showerror("Error", "El identificador ya existe")
+                return False
+            return True
+        except ValueError:
+            messagebox.showerror("Error", "El número de contacto debe ser un valor numérico válido.")
             return False
-        if not numero_contacto.isdigit() or not 1000000 < int(numero_contacto) < 100000000:
-            messagebox.showerror("Error", "El número de contacto debe ser un número debe tener 8 dígitos")
+        except Exception as e:
+            messagebox.showerror("Error", f"Ha ocurrido un error inesperado: {str(e)}")
             return False
-        if identificador in self.identificadores_existentes:
-            messagebox.showerror("Error", "El identificador ya existe")
-            return False
-        return True
     
     def crear_centro_acopio(self, sede, numero_contacto, identificador):
         self.validar_input_centro_acopio(sede, numero_contacto, identificador)
@@ -34,6 +41,9 @@ class CentrosAcopio:
         return True
     
     def guardar_centro_acopio_base_datos(self):
-        with open('./base_datos/centrosacopio.txt','a') as file:
-            for centro_acopio in self.centros_acopio:
-                file.write(f"{centro_acopio}\n")
+        try:
+            with open('./base_datos/centrosacopio.txt', 'a') as file:
+                for centro_acopio in self.centros_acopio:
+                    file.write(f"{centro_acopio}\n")
+        except Exception as e:
+            messagebox.showerror("Error", f"Ha ocurrido un error al guardar en la base de datos: {str(e)}")
