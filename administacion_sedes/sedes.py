87ae99aa475b9ac540fc98d6869f876266392f1e
Th3Elohim2208 <106294046+Th3Elohim2208@users.noreply.github.com>
2024-05-12 19:24:06 -0600

    se agrego la función de guardar datos de sede

--- administacion_sedes/sedes.py
+++ administacion_sedes/sedes.py
@@ -37,8 +37,17 @@ class Sedes:
             "Estado": estado
         }
         self.agregar_sede(sede)
+        self.guardar_sede_base_datos()
         return True
     
     def agregar_sede(self, sede):
         self.sedes.append(sede)
         return True
+    
+    def guardar_sede_base_datos(self):
+        try:
+            with open('./base_datos/sedes.txt', 'a') as file:
+                for sede in self.sedes:
+                    file.write(f"{sede}\n")
+        except Exception as e:
+            messagebox.showerror("Error", f"Ha ocurrido un error al guardar en la base de datos: {str(e)}")
