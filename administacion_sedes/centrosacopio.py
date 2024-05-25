93e053f1344844b589b26eccb7945c8484b3b547
Th3Elohim2208 <106294046+Th3Elohim2208@users.noreply.github.com>
2024-05-25 02:26:29 -0600

    error de duplicado de centros de acopio corregido

--- administacion_sedes/centrosacopio.py
+++ administacion_sedes/centrosacopio.py
@@ -6,15 +6,14 @@ class CentrosAcopio:
         self.sedes_existentes = set()
         self.identificadores_existentes = set()
         self.cargar_sedes_desde_archivo()
-    
-    #
+
     def validar_input_centro_acopio(self, sede, numero_contacto, identificador):
         try:
-            if not sede in self.sedes_existentes:
+            if sede not in self.sedes_existentes:
                 messagebox.showerror("Error", "La sede no existe")
                 return False
-            if not numero_contacto.isdigit() or not 1000000 < int(numero_contacto) < 100000000:
-                messagebox.showerror("Error", "El número de contacto debe ser un número debe tener 8 dígitos")
+            if not numero_contacto.isdigit() or not 10000000 <= int(numero_contacto) <= 99999999:
+                messagebox.showerror("Error", "El número de contacto debe ser un número de 8 dígitos")
                 return False
             if identificador in self.identificadores_existentes:
                 messagebox.showerror("Error", "El identificador ya existe")
@@ -26,8 +25,11 @@ class CentrosAcopio:
         except Exception as e:
             messagebox.showerror("Error", f"Ha ocurrido un error inesperado: {str(e)}")
             return False
-    
+
     def crear_centro_acopio(self, sede, numero_contacto, identificador):
+        if not self.validar_input_centro_acopio(sede, numero_contacto, identificador):
+            return False
+        
         centro_acopio = {
             "Sede": sede,
             "Número de contacto": numero_contacto,
@@ -36,12 +38,13 @@ class CentrosAcopio:
         self.agregar_centro_acopio(centro_acopio)
         self.guardar_centro_acopio_base_datos()
         return True
-    
+
     def agregar_centro_acopio(self, centro_acopio):
         self.centros_acopio.append(centro_acopio)
+        self.sedes_existentes.add(centro_acopio["Sede"])
+        self.identificadores_existentes.add(centro_acopio["Identificador"])
         return True
-    
-    # separara la parte de almacenamiento de la parte de logica
+
     def guardar_centro_acopio_base_datos(self):
         try:
             with open('./base_datos/centrosacopio.txt', 'a') as file:
@@ -50,7 +53,7 @@ class CentrosAcopio:
                     file.write(centro_acopio_str)
         except Exception as e:
             messagebox.showerror("Error", f"Ha ocurrido un error al guardar en la base de datos: {str(e)}")
-    
+
     def cargar_sedes_desde_archivo(self):
         try:
             with open('./base_datos/sedes.txt', 'r') as file:
@@ -60,3 +63,7 @@ class CentrosAcopio:
                     self.sedes_existentes.add(nombre)
         except Exception as e:
             messagebox.showerror("Error", f"Ha ocurrido un error al cargar las sedes desde el archivo: {str(e)}")
+
+
+
+
