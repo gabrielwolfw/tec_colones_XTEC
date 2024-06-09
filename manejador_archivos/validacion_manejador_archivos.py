ac72ad7f24acfcfbdff2f25de6d0a0461caad8c3
gabrielwolfw <kevinlobo1920k@gmail.com>
2024-06-09 04:47:11 -0600

    se implementó la conexión con la API de cuenta tec

--- manejador_archivos/validacion_manejador_archivos.py
+++ manejador_archivos/validacion_manejador_archivos.py
@@ -1,23 +1,13 @@
 import os
-from tkinter import messagebox
 
 def validar_existencia_archivo_transacciones():
-    if not os.path.exists("./base_datos/transacciones.txt"):
-        messagebox.showerror("Error", "El archivo transacciones.txt no existe. Por favor, comuníquese con el administrador del sistema.")
-        return
+    return os.path.exists("./base_datos/transacciones.txt")
 
 def validar_existencia_archivo_centros_acopio():
-    if not os.path.exists("./base_datos/centrosacopio.txt"):
-        messagebox.showerror("Error", "El archivo centrosacopio.txt no existe. Por favor, comuníquese con el administrador del sistema.")
-        return
+    return os.path.exists("./base_datos/centrosacopio.txt")
 
 def validar_existencia_archivo_materiales():
-    if not os.path.exists("./base_datos/materiales.txt"):
-        messagebox.showerror("Error", "El archivo materiales.txt no existe. Por favor, comuníquese con el administrador del sistema.")
-        return
-    
-def validar_existneia_archivo_sedes():
-    if not os.path.exists("./base_datos/sedes.txt"):
-        messagebox.showerror("Error", "El archivo sedes.txt no existe. Por favor, comuníquese con el administrador del sistema.")
-        return False
-    return True
+    return os.path.exists("./base_datos/materiales.txt")
+
+def validar_existencia_archivo_sedes():
+    return os.path.exists("./base_datos/sedes.txt")
